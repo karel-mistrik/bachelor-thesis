fa8b84a93f8d043064bc903afb3192d620adf319
Karel Mistrík <mistrik.karel@gmail.com>
2023-06-04 18:19:34 +0200

    Tailwind font configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      primary: "#000000",
+      secondary: "#FF0000",
+      background: "#ffffff",
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      gelasio: ["Gelasio", "serif"],
+    },
+    fontSize: {
+      title: "10rem",
+      navigation: "0.75rem",
+      chapter: "1.25rem",
+      text: "0.938rem",
+    },
     extend: {},
   },
   plugins: [],
